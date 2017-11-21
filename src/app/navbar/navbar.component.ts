4fa88551042a189476623b83a991fe293cf48137
Henry Zarza <henryacz7@gmail.com>
2017-11-20 19:36:35 -0500

    Corrections in the structuring of some files

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,7 +6,6 @@ declare let jQuery: any;
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class NavbarComponent implements OnInit {
