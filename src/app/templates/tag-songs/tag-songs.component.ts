1f0d2dc04d13015e9e0d43c4bf7f9b998e3ce74e
Henry Zarza <henryacz7@gmail.com>
2017-11-25 18:55:14 -0500

    Finalization of component tag-songs

--- src/app/templates/tag-songs/tag-songs.component.ts
+++ src/app/templates/tag-songs/tag-songs.component.ts
@@ -1,6 +1,8 @@
+import { CrudService } from './../../services/crud.service';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 declare var jQuery: any;
+declare var Dropzone: any;
 
 @Component({
   selector: 'app-tag-songs',
@@ -9,10 +11,35 @@ declare var jQuery: any;
 })
 export class TagSongsComponent implements OnInit {
 
-  constructor() { }
+  public data: any;
+  public registerEdit: any;
+
+  constructor(private crud: CrudService) {
+    Dropzone.autoDiscover = false;
+  }
 
   ngOnInit() {
+    this.crud.get('tag-songs.json').subscribe(
+      response => this.data = response,
+      error => console.log(error)
+    );
+    jQuery('#modalEdit').modal();
     jQuery('.tooltipped').tooltip({delay: 50});
+  }
+
+  private openModal(tag) {
+    this.registerEdit = tag;
+    jQuery('#modalEdit').modal('open');
+    setTimeout(() => this.inicializateDropzone(), 200);
+  }
+
+  private inicializateDropzone() {
+    jQuery('#dropzone-img-tag').dropzone({
+      url: '/assets/users',
+      maxFiles: 1,
+      acceptedFiles: 'image/*',
+      addRemoveLinks: true
+    });
   }
 
 }
