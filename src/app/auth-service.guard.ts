57cf578d2b8a4426f29933366c8d892017dd0800
Henry Zarza <henryacz7@gmail.com>
2017-12-02 17:40:51 -0500

    Finalization demo for Proyecto Integrador

--- src/app/auth-service.guard.ts
+++ src/app/auth-service.guard.ts
@@ -1,16 +1,28 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthServiceGuard implements CanActivate {
+
+  constructor(private router: Router) {}
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
 
-      const isLogged = localStorage.getItem('isLogged');
       if (state.url !== '/') {
-        return (isLogged || isLogged === 'true') ? true : false;
+        const isLogged = localStorage.getItem('isLogged');
+
+        if (isLogged || isLogged === 'true') {
+          // this.router.navigate([state.url]);
+          return true;
+        } else {
+          this.router.navigate(['/login']);
+        }
+        return false;
+        // return (isLogged || isLogged === 'true') ? true : false;
       }
       return true;
   }
